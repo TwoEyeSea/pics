e9d9109bfe6993fe28dd931c03f9d8ada42eb4fe
M Alexander Moroney <alexmoroney@gmail.com>
2022-07-17 16:16:22 -0500

    L82 - Uncontrolled vs Controlled Elements

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,13 +1,9 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    // This is the convention for defining event handler callback function
-    // "on" to indicate that this callback function corresponds to an event handler
-    // "input" the element that using the event handler on
-    // "Change" the type of event handler that we're utiilizing for this callback function
-    // Some people will use "handleInputChange" instetad
-  }
+  state = {
+    term: "",
+  };
 
   onInputClick() {
     console.log("Input was clicked");
@@ -19,7 +15,12 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" placeholder="booya" onChange={(e) => console.log(e.target.value)} />
+            <input
+              type="text"
+              placeholder="booya"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
             {/* Notice that the "this.onInputChange" callback function isn't defined with a set of parentheses "()",  */}
             {/* this is because we don't want to call the function on startUp of the application. This way allows us to call the onInputChange function at a later time  */}
             {/* MAIN TAKEAWAY -> We do not add parentheses when we pass a callback function to onChange */}
