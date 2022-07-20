5644299b664eb8724b3a862ddb18578188127693
M Alexander Moroney <alexmoroney@gmail.com>
2022-07-19 19:30:08 -0500

    L95 - Setting State After Async Requests

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,19 +3,29 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] };
+  // We default the images prop to be an empty array because we receive the image results data from the API in the form of an array
+  // this allows us to use methods associated with arrays
+
+  async onSearchSubmit(term) {
+    //Async keyword in front of the event handler indicates that we're going to use async syntax within the function
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID uyojAcvL3i5GzARBDhnFQlQ34joOii7ZE3qUkAjKqjo",
       },
     });
-    console.log(term);
+    console.log(response.data.results);
+    // Displays the results array within the console
+    console.log(this);
+    // Displaying "this" on the console to determine what it's representing
+    this.setState({ images: [response.data.results] });
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
+        <SearchBar onSubmit={(e) => this.onSearchSubmit(e)} />
+        Found: {this.state.images.length}
       </div>
     );
   }
